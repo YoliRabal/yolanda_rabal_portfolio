e76eab3717c6e473436a5e2e2e7e6b0df16b8872
YoliRabal <yoli.rabal.adan@gmail.com>
2026-07-14 19:56:36 +0200

    Update GitHub Pages build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "676592821" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1651851506" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
